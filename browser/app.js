f7e4f508ac48804da359661269a8d0551f263cd2
leilabl <leilaloezer@gmail.com>
2016-02-09 13:04:06 -0500

    sockets connecting and disconneting

---
+++ browser/app.js
@@ -0,0 +1,16 @@
+// Never seen window.location before?
+// This object describes the URL of the page we're on!
+var socket = io(window.location.origin);
+
+socket.on('connect', function () {
+    console.log('I have made a persistent two-way connection to the server!');
+});
+
+
+
+
+
+whiteboard.on('draw', function(drawing) {
+	//console.log('my drawing', drawing);
+
+})
--- server.js
+++ server.js
@@ -2,11 +2,30 @@ var path = require('path');
 
 var http = require('http');
 var server = http.createServer();
+//because we are using sockets we need to create a server with http
+var socketio = require('socket.io');
 
 var express = require('express');
 var app = express();
 
+//this line needs to be above creating the io server
 server.on('request', app);
+
+var io = socketio(server);
+
+io.on('connection', function (socket) {
+	console.log('a new client is connected')
+	console.log(socket.id)
+  // socket.emit('draw', { hello: 'world' });
+  // socket.on('my other event', function (data) {
+  //   console.log(data);
+  // });
+	socket.on('disconnect', function () {
+	    console.log('Goodbye :(');
+	});
+});
+
+
 
 server.listen(1337, function () {
     console.log('The server is listening on port 1337!');
